d1c4705c5ffa738e396a9c9c5974084b3e138270
Nizshime-Samara <samaramathildes@gmail.com>
2023-11-05 15:15:52 -0300

    Refatorando UserResouceController para receber metodo GET e media types

--- src/main/java/org/estudos/nizshime/camada_de_apresentação/UserResourceController.java
+++ src/main/java/org/estudos/nizshime/camada_de_apresentação/UserResourceController.java
@@ -1,5 +1,7 @@
 package org.estudos.nizshime.camada_de_apresentação;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -9,13 +11,18 @@ import jakarta.ws.rs.core.Response;
 import org.estudos.nizshime.camada_de_dominio.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResourceController {
 
     @POST
-    @Produces(MediaType.APPLICATION_JSON)
     public Response createUser(CreateUserRequest CreateUserRequest) {
         return Response.ok().build();
+    }
 
+    @GET
+    public Response listAllUsers() {
+        return Response.ok().build();
     }
 
 }
