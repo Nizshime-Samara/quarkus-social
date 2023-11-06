6be25f18802abc8c25a6ae82957ed7aa4a4cbc01
Nizshime-Samara <samaramathildes@gmail.com>
2023-11-05 21:14:20 -0300

    Mapeando entidade Users

---
+++ src/main/java/org/estudos/nizshime/camada_de_dominio/model/User.java
@@ -0,0 +1,83 @@
+package org.estudos.nizshime.camada_de_dominio.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    public User(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private Integer age;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    // Define uma noção de igualdade de objeto
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    // Retorna um valor de hash para o objeto, que é usado em algumas coleções para
+    // encontrar objetos eficientemente
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    // Representaçao para o estado do objeto
+    @Override
+    public String toString() {
+        return "{" +
+                " id='" + getId() + "'" +
+                ", name='" + getName() + "'" +
+                ", age='" + getAge() + "'" +
+                "}";
+    }
+}
