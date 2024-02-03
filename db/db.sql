2dfa8f3af54adbdd0c4ac6ada9c93b746e474601
Nizshime-Samara <samaramathildes@gmail.com>
2024-02-03 16:09:54 -0300

    Configuração inicial de docker-compose para imagem de BD e app

--- db/db.sql
+++ db/db.sql
@@ -1,19 +1,20 @@
-create DATABASE quarkus-social; 
+CREATE DATABASE IF NOT EXISTS `quarkus-social` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE quarkus_social;
 
-CREATE TABLE users (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  age INT NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `posts` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `text_post` varchar(150) NOT NULL,
+  `date_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE posts (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  text_post VARCHAR(150) NOT NULL,
-  date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  user_id INT UNSIGNED NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `age` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/java/org/estudos/nizshime/QuarkusSocialApplication.java
+++ src/main/java/org/estudos/nizshime/QuarkusSocialApplication.java
@@ -7,7 +7,13 @@ import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
 
-@OpenAPIDefinition(info = @Info(title = "Quarkus Social API", version = "1.0.0", contact = @Contact(name = "Nizshime Samara", url = "https://github.com/Nizshime-Samara/quarkus-social", email = "samaramathildes@gmail.com"), license = @License(name = "Apache 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
+@OpenAPIDefinition(
+    info = @Info(title = "Quarkus Social API", version = "1.0.0", 
+    contact = @Contact(name = "Nizshime Samara", 
+    url = "https://github.com/Nizshime-Samara/quarkus-social", 
+    email = "samaramathildes@gmail.com"), 
+    license = @License(name = "Apache 2.0", 
+    url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
 
 )
 public class QuarkusSocialApplication extends Application {
